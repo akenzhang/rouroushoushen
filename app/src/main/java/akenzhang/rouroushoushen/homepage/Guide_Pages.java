6c094b635f001a79a7d2f994639696423ba9c793
windwhere <sjr1175300866@163.com>
2016-06-27 10:28:59 +0800

    ffff

---
+++ app/src/main/java/akenzhang/rouroushoushen/homepage/Guide_Pages.java
@@ -0,0 +1,15 @@
+package akenzhang.rouroushoushen.homepage;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import akenzhang.rouroushoushen.R;
+
+public class Guide_Pages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide__pages);
+    }
+}
---
+++ app/src/main/java/akenzhang/rouroushoushen/homepage/st1.java
@@ -0,0 +1,13 @@
+package akenzhang.rouroushoushen.homepage;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/6/27.
+ */
+public class st1 extends AppCompatActivity {
+
+
+
+
+}
