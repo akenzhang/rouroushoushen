ebd2b292e4c4d41e99222edf7ed28bbc8234f988
windwhere <sjr1175300866@163.com>
2016-06-27 10:45:53 +0800

    sjr

--- app/src/main/java/akenzhang/rouroushoushen/homepage/Guide_Pages.java
+++ app/src/main/java/akenzhang/rouroushoushen/homepage/Guide_Pages.java
@@ -1,15 +1,41 @@
 package akenzhang.rouroushoushen.homepage;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import akenzhang.rouroushoushen.R;
+import akenzhang.rouroushoushen.ui.BaseActivity;
 
-public class Guide_Pages extends AppCompatActivity {
+public class Guide_Pages extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_guide__pages);
+        setContentView(getLayout());
+
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_guide__pages;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initVariable() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void bindData() {
+
     }
 }
