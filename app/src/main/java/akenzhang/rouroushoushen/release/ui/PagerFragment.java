0e521646f3b724852b6c535fe5a37272a1569413
Kevin <284264312@qq.com>
2016-06-27 13:07:15 +0800

    添加了fragment

---
+++ app/src/main/java/akenzhang/rouroushoushen/release/ui/PagerFragment.java
@@ -0,0 +1,34 @@
+package akenzhang.rouroushoushen.release.ui;
+
+import akenzhang.rouroushoushen.R;
+import akenzhang.rouroushoushen.ui.BaseFragment;
+
+/**
+ * Created by Administrator on 2016/6/27 0027.
+ */
+public class PagerFragment extends BaseFragment{
+    @Override
+    protected int getLayout() {
+        return R.layout.fragment_bottom;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initVariable() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void bindData() {
+
+    }
+}
