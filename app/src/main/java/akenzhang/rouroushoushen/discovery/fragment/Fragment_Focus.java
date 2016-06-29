bc0b9ff9d2080a21b33a349240d35e5c2c042d21
windwhere <sjr1175300866@163.com>
2016-06-29 09:41:06 +0800

    sjr - 6.29 9:31

---
+++ app/src/main/java/akenzhang/rouroushoushen/discovery/fragment/Fragment_Focus.java
@@ -0,0 +1,34 @@
+package akenzhang.rouroushoushen.discovery.fragment;
+
+import akenzhang.rouroushoushen.R;
+import akenzhang.rouroushoushen.ui.BaseFragment;
+
+/**
+ * Created by Administrator on 2016/6/28 0028.
+ */
+public class Fragment_Focus extends BaseFragment{
+    @Override
+    protected int getLayout() {
+        return R.layout.discovery_fragment_focus;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initVariable() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void bindData() {
+
+    }
+}
---
+++ app/src/main/java/akenzhang/rouroushoushen/discovery/fragment/Fragment_Group.java
@@ -0,0 +1,63 @@
+package akenzhang.rouroushoushen.discovery.fragment;
+
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import akenzhang.rouroushoushen.R;
+import akenzhang.rouroushoushen.discovery.adatper.AdvViewPagerAdapter;
+import akenzhang.rouroushoushen.ui.BaseFragment;
+
+/**
+ * Created by Administrator on 2016/6/28 0028.
+ */
+public class Fragment_Group extends BaseFragment{
+    private List<View> list ;
+    private ViewPager viewPager;
+    int[] res ={R.drawable.bg_newbie_board_boy,R.drawable.bg_newbie_board_girl};
+
+    @Override
+    protected int getLayout() {
+        return R.layout.discovery_fragment_group;
+    }
+
+    @Override
+    protected void initView() {
+        viewPager= (ViewPager) root.findViewById(R.id.group_advview);
+        bindData();
+        AdvViewPagerAdapter advViewPagerAdapter=new AdvViewPagerAdapter(list);
+        viewPager.setAdapter(advViewPagerAdapter);
+
+    }
+
+    @Override
+    protected void initVariable() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void bindData() {
+        list =new ArrayList<>();
+        ViewGroup.LayoutParams layoutParams =new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        for (int i=0 ; i< res.length ;i++){
+            ImageView imageView =new ImageView(getActivity());
+            imageView.setImageResource(res[i]);
+            imageView.setLayoutParams(layoutParams);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+
+            list.add(imageView);
+
+
+        }
+
+    }
+}
