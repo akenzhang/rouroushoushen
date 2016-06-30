e94bb9806d0fb7148148a6bd7113746293d3d9ad
akenzhang <372891240@qq.com>
2016-06-30 15:28:06 +0800

    user register and login via QQ

--- app/src/main/java/akenzhang/rouroushoushen/ui/Login.java
+++ app/src/main/java/akenzhang/rouroushoushen/ui/Login.java
@@ -1,17 +1,24 @@
 package akenzhang.rouroushoushen.ui;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.Image;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.squareup.okhttp.Request;
 import com.tencent.connect.UserInfo;
 import com.tencent.connect.common.Constants;
 import com.tencent.open.utils.HttpUtils;
@@ -32,37 +39,113 @@ import akenzhang.rouroushoushen.R;
  */
 public class Login extends AppCompatActivity{
 
-    private Button btn_qq;
     //QQ登陆
     private static String APP_ID="1105434609";
     private Tencent mTencent;
     private UserInfo mUserInfo;
 
+    private EditText edloginphone;
+    private EditText edloginpwd;
+
+    private TextView tvLogin;
+    private TextView tvRegister;
+    private ImageView ivRegisterqq;
+
+    //id_login
+    //id_register
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logon);
 
-        btn_qq = (Button) this.findViewById(R.id.btn_qq);
+        edloginphone = (EditText) this.findViewById(R.id.id_login_phone);
+        edloginpwd = (EditText) this.findViewById(R.id.id_login_pwd);
+        ivRegisterqq = (ImageView) this.findViewById(R.id.id_register_qq);
+
+        ivRegisterqq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTencent.login(Login.this, "get_user_info", new LoginUiListener());
+            }
+        });
+
+
+        tvLogin = (TextView) this.findViewById(R.id.id_login);
+        tvRegister = (TextView) this.findViewById(R.id.id_register);
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
 
         // Tencent类是SDK的主要实现类，开发者可通过Tencent类访问腾讯开放的OpenAPI。
         // 其中APP_ID是分配给第三方应用的appid，类型为String。
         if(mTencent==null) {
             mTencent = Tencent.createInstance(APP_ID, this.getApplicationContext());
         }
-        btn_qq.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!mTencent.isSessionValid())
-                {
-                    mTencent.login(Login.this, "get_user_info", new LoginUiListener());
-                }
-            }
-        });
 
     }
     public void login(View view){
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+
+        //点击登录按钮，进行用户名和密码的验证
+        final String strLoginPhone = edloginphone.getText().toString();
+        String strPwd = edloginpwd.getText().toString();
+
+       if(strLoginPhone.equals("") )
+        {
+            Toast.makeText(Login.this,"用户名不能为空~",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if( strPwd.equals(""))
+        {
+            Toast.makeText(Login.this,"密码不能为空~",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(!TextUtils.isDigitsOnly(strLoginPhone))
+        {
+            Toast.makeText(Login.this,"手机号码不能带字符~",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        //调用OkHttpUtils获取网络资源
+        String requestUrl = "http://www.1316818.com/jsonserver.aspx?username="+ strLoginPhone +"&password="+ strPwd +"";
+        OkHttpUtils.getAsync(requestUrl, new OkHttpUtils.DataCallBack() {
+            @Override
+            public void requestFailure(Request request, IOException e) {}
+
+            @Override
+            public void requestSuccess(String result) {
+
+                try {
+                    JSONObject jsonObject = new JSONObject(result);
+                    int intStatus = jsonObject.getInt("status");
+
+                    if(intStatus==1){
+
+                        //将获取到的用户信息保存起来
+                        SharedPreferences mySharedPreferences = getSharedPreferences("akenzhang", Activity.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = mySharedPreferences.edit();
+                        editor.putString("nickname",strLoginPhone );
+                        editor.putString("image","http://q.qlogo.cn/qqapp/1105434609/3AB2E5A88E27ACFE39CE6AB4BE4194CE/100" );
+                        editor.putString("gender","未知" );
+                        editor.commit();
+
+                        Intent intent = new Intent(Login.this, MainActivity.class);
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(Login.this,"用户名或者密码不正确，登陆失败！",Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/akenzhang/rouroushoushen/ui/RegisterActivity.java
@@ -0,0 +1,230 @@
+package akenzhang.rouroushoushen.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.okhttp.Request;
+import com.tencent.connect.UserInfo;
+import com.tencent.connect.common.Constants;
+import com.tencent.tauth.IUiListener;
+import com.tencent.tauth.Tencent;
+import com.tencent.tauth.UiError;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import akenzhang.rouroushoushen.MainActivity;
+import akenzhang.rouroushoushen.R;
+
+/**
+ * Created by Administrator on 2016/6/30.
+ */
+public class RegisterActivity extends BaseActivity {
+
+    private TextView tvLogin;
+    private TextView tvRegister;
+    private Button btnRegister;
+    private EditText edLoginPhone;
+    private EditText edLoginPwd;
+    private ImageView ivRegisterqq;
+
+    //QQ登陆
+    private static String APP_ID="1105434609";
+    private Tencent mTencent;
+    private UserInfo mUserInfo;
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_register;
+    }
+
+    @Override
+    protected void initView() {
+        tvLogin = (TextView) this.findViewById(R.id.id_login);
+        tvRegister = (TextView) this.findViewById(R.id.id_register);
+        btnRegister = (Button) this.findViewById(R.id.btn_register);
+        edLoginPhone = (EditText) this.findViewById(R.id.id_login_phone);
+        edLoginPwd = (EditText) this.findViewById(R.id.id_login_pwd);
+        ivRegisterqq = (ImageView) this.findViewById(R.id.id_register_qq);
+
+    }
+
+    @Override
+    protected void initVariable() {
+
+        if(mTencent==null) {
+            mTencent = Tencent.createInstance(APP_ID, this.getApplicationContext());
+        }
+    }
+
+    @Override
+    protected void initListener() {
+
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //这里写入注册事件的逻辑
+                String strLoginPhone = edLoginPhone.getText().toString();
+                String strPwd = edLoginPwd.getText().toString();
+
+                if(strLoginPhone.equals("") )
+                {
+                    Toast.makeText(RegisterActivity.this,"手机号码不能为空~",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if( strPwd.equals(""))
+                {
+                    Toast.makeText(RegisterActivity.this,"密码不能为空~",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(!TextUtils.isDigitsOnly(strLoginPhone))
+                {
+                    Toast.makeText(RegisterActivity.this,"手机号码不能带字符~",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                String requestUrl = "http://www.1316818.com/jsonserver.aspx?nusername="+ strLoginPhone +"&npassword="+ strPwd +"";
+                OkHttpUtils.getAsync(requestUrl, new OkHttpUtils.DataCallBack() {
+                    @Override
+                    public void requestFailure(Request request, IOException e) {}
+
+                    @Override
+                    public void requestSuccess(String result) {
+                        try {
+                            JSONObject json = new JSONObject(result);
+                            String strUsername = json.getString("username");
+
+                            //将获取到的用户信息保存起来
+                            SharedPreferences mySharedPreferences = getSharedPreferences("akenzhang", Activity.MODE_PRIVATE);
+                            SharedPreferences.Editor editor = mySharedPreferences.edit();
+                            editor.putString("nickname",strUsername );
+                            editor.putString("image","http://q.qlogo.cn/qqapp/1105434609/3AB2E5A88E27ACFE39CE6AB4BE4194CE/100" );
+                            editor.putString("gender","未知" );
+                            editor.commit();
+
+                            //登录后跳转
+                            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                            startActivity(intent);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+
+        //QQ自动登录
+        ivRegisterqq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTencent.login(RegisterActivity.this, "get_user_info", new LoginUiListener());
+            }
+        });
+
+    }
+
+    @Override
+    protected void bindData() {}
+
+    //实现QQ登陆的回调接口
+    class LoginUiListener implements IUiListener {
+
+        @Override
+        public void onComplete(Object o) {
+            JSONObject jsonObject = (JSONObject)o;
+
+            try {
+                String openid = jsonObject.getString("openid");
+                String expires_in = jsonObject.getString("expires_in");
+                String token = jsonObject.getString("access_token");
+                if (!TextUtils.isEmpty(openid)&&!TextUtils.isEmpty(expires_in)&&!TextUtils.isEmpty(token)){
+                    mTencent.setOpenId(openid);
+                    mTencent.setAccessToken(token,expires_in);
+
+                    //拿到QQ用户的数据
+                    mUserInfo = new UserInfo(RegisterActivity.this,mTencent.getQQToken());
+                    mUserInfo.getUserInfo(new IUiListener() {
+                        @Override
+                        public void onComplete(Object o) {
+                            JSONObject json = (JSONObject)o;
+
+                            try {
+                                String nickname = json.getString("nickname");
+                                String image = json.getString("figureurl_qq_2");
+                                String gender = json.getString("gender");
+
+                                //将获取到的用户信息保存起来
+                                SharedPreferences mySharedPreferences = getSharedPreferences("akenzhang", Activity.MODE_PRIVATE);
+                                SharedPreferences.Editor editor = mySharedPreferences.edit();
+                                editor.putString("nickname",nickname );
+                                editor.putString("image",image );
+                                editor.putString("gender",gender );
+                                editor.commit();
+
+                                //登录后跳转
+                                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                startActivity(intent);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onError(UiError uiError) {
+                            String strMsg = uiError.errorMessage;
+                        }
+
+                        @Override
+                        public void onCancel() {}
+                    });
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onError(UiError e) {
+
+            Toast.makeText(RegisterActivity.this,e.errorMessage,Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onCancel() {}
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == Constants.REQUEST_LOGIN) {
+            mTencent.onActivityResultData(requestCode, resultCode, data, new LoginUiListener());
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
